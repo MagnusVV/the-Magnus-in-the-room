db098e072473f86951d06d636b6acca3ecbae212
MagnusVV <svartkub@msn.com>
2022-10-29 12:53:05 +0200

    Added a meta description to the header.php.

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Pictures of my cats, guaranteed to make you smile a little.">
     <title>My cats!</title>
     <link rel="stylesheet" href="./style.css">
     <link rel="icon" href="./images/Logga_Magnus_SV.ico">
